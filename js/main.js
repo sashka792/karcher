f8800f700002d15d3f84860ec096c91e8178c958
sashka792 <sashka792@gmail.com>
2019-07-21 23:28:45 +0300

    pixel perfect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 ;(function() {
 	"use strict";
-
+	
+	// $('.ba-slider-comments').slick({
+	// 	dots: false,
+	// 	arrows: true,
+	// 	slide: '.ba-works-slide',
+	// 	slidesToShow: 1,
+	// 	slidesToScroll: 1,
+	// 	dots: rue,
+	// 	autoplay: true,
+	// 	// prevArrow: '.ba-slider--prev',
+	// 	// nextArrow: '.ba-slider--next'
+	// })
+	
 })();
 
+$('.ba-slider-comments').slick({
+	dots: false,
+	arrows: false,
+	slide: '.ba-works-slide',
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	// prevArrow: '.ba-slider--prev',
+	// nextArrow: '.ba-slider--next'
+})
